97696e00cde6c76b5de97438b7e14e76f44ce7a0
Miruna <mirumuresan.13@gmail.com>
2025-11-02 16:27:21 +0200

    add url

--- frontend/app.py
+++ frontend/app.py
@@ -3,9 +3,11 @@ import requests
 
 st.title("Frontend Streamlit")
 
+backend_url = "https://cc2025-api.azurewebsites.net/api/data"
+
 try:
-    response = requests.get("http://127.0.0.1:8000/api/data")
+    response = requests.get(backend_url)
     data = response.json()
     st.success(f"Mesaj de la backend: {data['message']}")
 except Exception as e:
-    st.error("Nu s-a putut conecta la backend")
+    st.error(f"Nu s-a putut conecta la backend: {e}")
